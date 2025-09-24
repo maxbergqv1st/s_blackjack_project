cbd9c3d3944da87d67c8e5cd9c70d5a966d9aba8
unknown <mjmaxbq@icloud.com>
2025-09-24 12:23:55 +0200

    added enum menu system

---
+++ MenuOption.cs
@@ -0,0 +1,9 @@
+namespace App;
+
+enum MenuOption
+{
+      StartGame = 1,
+      ShowRules = 2,
+      ShowDeck = 3,
+      Exit = 4,
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, github!");
+﻿using App;
 
 List<string> suit_cards = new List<string>{"♠","♥","♦","♣",};
 
@@ -16,13 +14,59 @@ foreach(var suit in suit_cards)
       }
 }
 
+
+
 bool running = true;
 
 while(running)
 {
-      foreach(var deck in deck_list)
+      Console.Clear();
+      Console.WriteLine("===== BLACKJACK GAME  =====");
+      Console.WriteLine("\n1. Play blackjack");
+      Console.WriteLine("\n2. Rules of blackjack");
+      Console.WriteLine("\n3. Show deck of cards");
+      Console.WriteLine("\n4. Exit");
+
+      string? input = Console.ReadLine();
+
+      if(int.TryParse(input, out int option) && Enum.IsDefined(typeof(MenuOption), option))
       {
-            Console.WriteLine(deck);
+            MenuOption menuChoice = (MenuOption)option;
+            switch(menuChoice)
+            {
+            case MenuOption.StartGame : 
+                  Console.Clear();
+                  Console.WriteLine("Play..");
+                  
+                  Console.ReadLine();
+            break;
+            case MenuOption.ShowRules : 
+                  Console.Clear();
+                  Console.WriteLine("Rules..");
+                  
+                  Console.ReadLine();
+            break;  
+            case MenuOption.ShowDeck : 
+                  foreach(var deck in deck_list)
+                  {
+                        Console.WriteLine(deck);
+                  }
+                  Console.ReadLine();
+            break;  
+            case MenuOption.Exit : 
+                  Console.Clear();
+                  Console.WriteLine("Exiting....");
+                  running = false;
+                  Console.ReadLine();
+                  Console.Clear();
+            break;        
+            }
       }
-      Console.ReadLine();
+      else
+      {
+            Console.Clear();
+            Console.WriteLine("invalid option... PRESS enter to continue");
+            Console.ReadLine();
+      }
 }
+
