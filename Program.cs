421215694c7b008a33e87c1576c3293c3abf6221
unknown <mjmaxbq@icloud.com>
2025-09-24 11:58:33 +0200

    added deck of cards with suits and ranks

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, github!");
+
+List<string> suit_cards = new List<string>{"♠","♥","♦","♣",};
+
+List<string> rank_cards = new List<string>{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+List<string> deck_list = new List<string>();
+
+foreach(var suit in suit_cards)
+{
+      foreach(var rank in rank_cards)
+      {
+            deck_list.Add(suit + rank);
+      }
+}
+
+bool running = true;
+
+while(running)
+{
+      foreach(var deck in deck_list)
+      {
+            Console.WriteLine(deck);
+      }
+      Console.ReadLine();
+}
